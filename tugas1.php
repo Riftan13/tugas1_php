3b78ab63293b06b466e0ddc1b9a0aa4a6549763b
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-10-07 10:25:00 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,26 @@
+<?php
+	$ayamGoreng = "Rp 13000";
+	$ayamBakar = "Rp 15000";
+	$ayamSayur = "Rp 13000";
+	$tempeGoreng = "Rp 1000";
+	$tahuGoreng = "Rp 1000";
+	$soto = "Rp 3000";
+	$nasiPutih = "Rp 5000";
+	$esTeh = "Rp 4000";
+	$esTebu = "Rp 5000";
+
+
+	echo "<h1>Daftar Menu</h1> <br>";
+	echo "<ul>
+			<li> Ayam Goreng    = ".$ayamGoreng."</li>
+		 	<li> Ayam Bakar 	= ".$ayamBakar."</li>
+		 	<li> Ayam Sayur 	= ".$ayamSayur."</li>
+		 	<li> Tempe Goreng 	= ".$tempeGoreng."</li>
+		 	<li> Tahu Goreng 	= ".$tahuGoreng."</li>
+		 	<li> Soto 			= ".$soto."</li>
+		 	<li> Nasi Putih 	= ".$nasiPutih."</li>
+		 	<li> Es Teh 		= ".$esTeh."</li>
+		 	<li> Es Tebu 		= ".$esTebu."</li>
+		  </ul>";
+
+?>
